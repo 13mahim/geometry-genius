d9caced4d3fd4be579094bd395d5005632edb9c1
13mahim <fahad13mahim@gmail.com>
2025-01-10 05:01:34 +0600

    rectangle

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -10,4 +10,12 @@ function calculateTriangleArea(){
     const triangleHeightText = triangleHeightInput.value;
     const height = parseFloat(triangleHeightText);
     console.log(height);
+
+    // calculate area
+    const area = 0.5 * base * height;
+    console.log("area of the triangle is:", area);
+
+    //display area
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
 }
