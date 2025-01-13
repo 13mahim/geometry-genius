3d23de7ffeab45ddf965ee00c6ac2f04007dbb20
13mahim <fahad13mahim@gmail.com>
2025-01-14 02:27:18 +0600

    bg-edit

--- scripts/Rectangle.js
+++ scripts/Rectangle.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea(){
-    console.log("calculateRectangleArea");
+    // get length of the rectangle
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const lengthText = rectangleLengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // get width of the rectangle
+    const rectangleWidthInput = document.getElementById("rectangle-width")
+    const widthText = rectangleWidthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    // calculate area
+    const area = length * width;
+    console.log("area of the rectangle is: ", area);
+
+    // display area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
